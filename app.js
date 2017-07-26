036398d4fd67fa0514fe8480b838d401fa2c97d4
verben-a <alina.verbenchuk@gmail.com>
2017-07-26 22:51:09 +0100

    Adding last changes

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
+var Youtube = function(videoId, videoUrl, videoTitle) {
+    this.videoId = videoId;
+    this.videoUrl = videoUrl;
+    this.videoTitle = videoTitle;
+}
+    // something in common (papa/mama)
+    // var Video = function(){}
+
+// // spoilt ones: sasha, marina, etc.
+// var Vimeo = function(){}
+// var Youtbe = function(){}
+// /other ideas?
+// prototypal method: 
+
+Youtube.prototype.generateHTML = function() {
+    // What is "this"?
+    return "<a href='https://www.youtube.com/watch?v=" + this.videoId + "'><img src=\"" + this.videoUrl + "\" style=\"default\"" + "</a><br><h3>" + this.videoTitle + "</h3>"
+}
+
+
+
 $(document).ready(function() {
 
+    // what properties
+    // - videoId
+    // - url
+    // - title
+    // define the Youtube object :-)
 
     $("form").submit(function(event) {
         event.preventDefault();
@@ -13,34 +39,17 @@ $(document).ready(function() {
         // console.log(request);
         $.getJSON("https://www.googleapis.com/youtube/v3/search", request).done(function(results) {
             $.each(results.items, function(index, item) {
-
-                $(".results").append("<img src=\"" + item.snippet.thumbnails.medium.url + "\" style=\"default\""
-                    + "<br>" + "<h3>" + item.snippet.title +"</h3>");
+                console.log(item);
+                    // instantiating or creating a new object for Youtube
+                var youtubeVideo = new Youtube(item.id.videoId, item.snippet.thumbnails.medium.url, item.snippet.title);                
+                // $(".results").append("<a href='https://www.youtube.com/watch?v=" + youtube.videoId + "'><img src=\"" + youtube.videoUrl + "\" style=\"default\"" + "</a><br><h3>" + youtube.videoTitle + "</h3>");
+                $('.results').append(youtubeVideo.generateHTML());
             });
 
         }).fail(function(data) {
 
         });
+
+
     });
 });
-
-// request.execute(function(response) {
-//     var str = JSON.stringify(response.result);
-//     $('#search-container').html('<pre>' + str + '</pre>');
-//   });
-
-
-//execute the request
-// request.execute(function(response) {
-//   var results = response.result;
-//   $.each(results.items, function(index,item) {
-//     console.log(item);
-//   });
-
-
-// function init(){
-//   gapi.client.setApiKey("AIzaSyDtVCBq96FsRaLCDibG_hNVMvJg_hwEMf4");
-//   gapi.client.load("youtube", "v3", function() {
-//     //yt api is ready
-//   });
-// }
